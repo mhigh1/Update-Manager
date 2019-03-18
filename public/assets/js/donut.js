c66cbe4ec5435eedc4ecc11d56dc59ca692af0f5
Genie Yang <genieyang93@gmail.com>
2019-03-18 16:11:56 -0400

    Adding more charts

--- public/assets/js/donut.js
+++ public/assets/js/donut.js
@@ -1,6 +1,22 @@
      $(document).ready( function() {
              
-            var donut1 = donutChart()
+            var donutDG = donutChart()
+            .width(200)
+            .height(180)
+            .cornerRadius(3) // sets how rounded the corners are on each slice
+            .padAngle(0.015) // effectively dictates the gap between slices
+            .variable('Percentage')
+            .category('Status');
+
+            var donutFW = donutChart()
+            .width(200)
+            .height(180)
+            .cornerRadius(3) // sets how rounded the corners are on each slice
+            .padAngle(0.015) // effectively dictates the gap between slices
+            .variable('Percentage')
+            .category('Status');
+
+            var donutUpdatesSum = donutChart()
             .width(200)
             .height(180)
             .cornerRadius(3) // sets how rounded the corners are on each slice
@@ -53,9 +69,22 @@
                     if (error) throw error;
                     d3.select('#chart1')
                         .datum(data) // bind data to the div
-                        .call(donut1) // draw chart in div
-                        // .call(donut2); // draw chart in div
+                        .call(donutDG) // draw chart in div
                 });
 
+        d3.tsv('/assets/js/servers.tsv', function(error, data) {
+            if (error) throw error;
+            d3.select('#chart2')
+                .datum(data) // bind data to the div
+                .call(donutFW) // draw chart in div
+        });
+
+        d3.tsv('/assets/js/servers.tsv', function(error, data) {
+            if (error) throw error;
+            d3.select('#chart3')
+                .datum(data) // bind data to the div
+                .call(donutUpdatesSum) // draw chart in div
+        });
+        
 
 });
