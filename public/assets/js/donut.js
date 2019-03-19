19889546c47128b49eadbdc534e08c1d88ec0eea
Genie Yang <genieyang93@gmail.com>
2019-03-19 12:42:14 -0400

    Adding new charts for FW

--- public/assets/js/donut.js
+++ public/assets/js/donut.js
@@ -14,7 +14,7 @@
             .cornerRadius(3) // sets how rounded the corners are on each slice
             .padAngle(0.015) // effectively dictates the gap between slices
             .variable('Percentage')
-            .category('Status');
+            .category('FiscalWeek');
 
             var donutUpdatesSum = donutChart()
             .width(200)
@@ -24,55 +24,23 @@
             .variable('Percentage')
             .category('Status');
 
-    //   var dataDetails = [
-    //                             {
-    //                             Status: "Installed",
-    //                             Percentage: "0.40",
-    //                             ServerCount: "0.045296463390387814"
-    //                             },
-    //                             {
-    //                             Status: "Needed",
-    //                             Percentage: "0.20",
-    //                             ServerCount: "0.045296463390387814"
-    //                             },
-    //                             {
-    //                             Status: "Downloaded",
-    //                             Percentage: "0.10",
-    //                             ServerCount: "0.045296463390387814"
-    //                             },
-    //                             {
-    //                             Status: "Pending",
-    //                             Percentage: "0.10",
-    //                             ServerCount: "0.045296463390387814"
-    //                             },
-    //                             {
-    //                             Status: "Failed",
-    //                             Percentage: ".05",
-    //                             ServerCount: "0.03390387814"
-    //                             },
-    //                                 {
-    //                             Status: "Unknown",
-    //                             Percentage: ".05",
-    //                             ServerCount: "0.03390387814"
-    //                             }
-    //                         ];
 
-
-    //     d3.json(dataDetails, function(error, data) {
-    //         if (error) throw error;
-    //             d3.select('#chart1')
-    //                 .datum(data) // bind data to the div
-    //                 .call(donut1) // draw chart in div
-    //     });
+    // Calling Datasets for each Donut
+    
+        // d3.json('/assets/js/servers.json', function(error, data) {
+        //     // d3.select('#chart1')
+        //     //   .data(data) // bind data to the div
+        //          console.log(data);
+        // });
 
         d3.tsv('/assets/js/servers.tsv', function(error, data) {
-                    if (error) throw error;
-                    d3.select('#chart1')
-                        .datum(data) // bind data to the div
-                        .call(donutDG) // draw chart in div
-                });
+            if (error) throw error;
+            d3.select('#chart1')
+                .datum(data) // bind data to the div
+                .call(donutDG) // draw chart in div
+        });
 
-        d3.tsv('/assets/js/servers.tsv', function(error, data) {
+        d3.tsv('/assets/js/fiscalweek.tsv', function(error, data) {
             if (error) throw error;
             d3.select('#chart2')
                 .datum(data) // bind data to the div
