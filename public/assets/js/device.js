3a04312bcd8a5c9abd93ac09a8f3d316c3f03366
Valentin Meica <valentinmeica@gmail.com>
2019-03-19 15:10:39 -0400

    updating tdd

--- public/assets/js/device.js
+++ public/assets/js/device.js
@@ -1,7 +1,3 @@
-// Used by deviceGroup.hbs //
-
-// SAMPLE DATA
-
 
 // SideNav -- Default Menu
 const tmplSideNav = () => {
@@ -17,10 +13,10 @@ const tmplSideNav = () => {
                             </a>
                         </li>
                         <li>
-                            <!--<a class="object" href="#">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="far fa-star"></i></span>
                                 <span class="object-navigator-label">Shortcuts</span>
-                            </a>-->
+                            </a>
                         </li>
                     </ul>
                 </li>
@@ -39,25 +35,25 @@ const tmplSideNav = () => {
                 <li>
                     <ul class="object-navigator-list">
                         <li>
-                            <a class="object" href="http://wsusdashboard.corporate.ge.com/wsus_dashboard/index.html"  target="_blank">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="fas fa-external-link-alt"></i></span>
                                 <span class="object-navigator-label">WSUS Dashboard</span>
                             </a>
                         </li>
                         <li>
-                            <a class="object" href="http://vdcgld03341.ics.cloud.ge.com/cgi-bin/selecttsl.cgi"  target="_blank">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="fas fa-external-link-alt"></i></span>
                                 <span class="object-navigator-label">Targeted Server List</span>
                             </a>
                         </li>
                         <li>
-                            <a class="object" href="http://patches.tsg.ge.com/standard/"  target="_blank">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="fas fa-external-link-alt"></i></span>
                                 <span class="object-navigator-label">Unix Status (Standard)</span>
                             </a>
                         </li>
                         <li>
-                            <a class="object" href="http://patches.tsg.ge.com/status/"  target="_blank">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="fas fa-external-link-alt"></i></span>
                                 <span class="object-navigator-label">Unix Status (Concierge)</span>
                             </a>
@@ -74,7 +70,7 @@ const tmplSideNav = () => {
                             </a>
                         </li>
                         <li>
-                            <a class="object" href="https://github.com/mhigh1/Update-Manager"  target="_blank">
+                            <a class="object" href="#">
                                 <span class="pr-1"><i class="fab fa-github"></i></span>
                                 <span class="object-navigator-label">GitHub Repository</span>
                             </a>
@@ -86,38 +82,18 @@ const tmplSideNav = () => {
     `;
 }
 
-// Document Ready
+
+
 $(document).ready(function() {
-
-    // Get the URL parameters
-    const urlParams = new URLSearchParams(window.location.search);
-
-    // Get device group from URL
-    $.get(`/api/deviceCollection/${urlParams.get('collectionId')}`)
-    .then(function(data) {
-        console.log(data);
-        $('#collectionName').text(data.Name);
-
-        // Get all devices with the device group
-        $.get(`/api/devices?collectionId=${urlParams.get('collectionId')}`)
-        .then(function(data) {
-            console.log(data);
-            
-            $('#deviceCount').text(data.length);
-        });
-    });
-
     
-
-
-
-    $('#devices').DataTable({
+    // Render the left-panel naviation
+    $("#left-panel").html(tmplSideNav());
+    
+    $('table#updates').DataTable({
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
         "language": {
             "search": "Filter:"
           }
     });
-
-    // Render the left-panel naviation
-    $("#left-panel").html(tmplSideNav());
+    
 });
--- sqlseeds/5.tblupdatestatusperdevices.sql
+++
@@ -1,7 +0,0 @@
-#alter table `valkyrie_db`.`tblupdatestatusperdevices` change pachageID packageID integer(11);
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('2', '1', 'Available');
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('2', '2', 'Available');
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('2', '3', 'Available');
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('3', '4', 'Staged');
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('3', '5', 'Installed');
-INSERT INTO `valkyrie_db`.`tblupdatestatusperdevices` (`deviceID`, `packageID`, `state`) VALUES ('1', '6', 'Available');
